ca12c57c6cadbdd5a6e25dfc5ce5ec514897d28f
albuquerque53 <gabrielalbuquerque-dev@hotmail.com>
2022-04-23 20:42:57 -0300

    feat: router for routes management

---
+++ app/router/router.go
@@ -0,0 +1,13 @@
+package router
+
+import (
+	"bookstore_api/app/controller/misc"
+	"log"
+	"net/http"
+)
+
+// Must handle all trafic of API
+func HandleRequests() {
+	http.HandleFunc("/health", misc.HealthCheck)
+	log.Fatal(http.ListenAndServe(":2001", nil))
+}
