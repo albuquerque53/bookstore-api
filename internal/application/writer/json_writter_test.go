311643a3a354ea9e9352207bf85a8f2da0885179
albuquerque53 <gabrielalbuquerque-dev@hotmail.com>
2022-05-03 19:11:17 -0300

    test: unit test for json_writter

---
+++ internal/application/writer/json_writter_test.go
@@ -0,0 +1,36 @@
+package writer
+
+import (
+	"fmt"
+	"testing"
+)
+
+type MockedJsonData struct {
+	Data MockedJsonMessage `json:"data"`
+}
+
+type MockedJsonMessage struct {
+	Message string `json:"message"`
+}
+
+func TestJSONString(t *testing.T) {
+	expectedMessage := "Hello, World!"
+
+	messageWriter := NewMessageWriter(expectedMessage)
+
+	jsonString, err := messageWriter.JSONString()
+
+	if err != nil {
+		t.Errorf("could not generate JSON by JSONString method by the following error: %v", err)
+	}
+
+	expectedJsonString := buildExpectedJson("Hello, World!")
+
+	if expectedJsonString != jsonString {
+		t.Errorf("the expected json (%v) does not match with returned (%v)", expectedJsonString, jsonString)
+	}
+}
+
+func buildExpectedJson(message string) string {
+	return fmt.Sprintf(`{"data":{"message":"%s"}}`, message)
+}
