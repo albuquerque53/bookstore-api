72767d638e2653e42dac86eaac9c71da5f3d9a92
albuquerque53 <gabrielalbuquerque-dev@hotmail.com>
2022-05-03 20:38:06 -0300

    test: json writer tests improve

--- internal/application/writer/json_writter_test.go
+++ internal/application/writer/json_writter_test.go
@@ -13,19 +13,30 @@ type MockedJsonMessage struct {
 	Message string `json:"message"`
 }
 
+type sceneries struct {
+	messageSent string
+}
+
 func TestJSONString(t *testing.T) {
-	expectedMessage := "Hello, World!"
-
-	jsonString, err := ToJSON(expectedMessage)
-
-	if err != nil {
-		t.Errorf("could not generate JSON by JSONString method by the following error: %v", err)
+	testSceneries := []sceneries{
+		{"hello world"},
+		{"Hello, World!"},
+		{"Olá, Mundo!"},
+		{"''"},
 	}
 
-	expectedJsonString := buildExpectedJson("Hello, World!")
+	for _, scenery := range testSceneries {
+		jsonString, err := ToJSON(scenery.messageSent)
 
-	if expectedJsonString != jsonString {
-		t.Errorf("the expected json (%v) does not match with returned (%v)", expectedJsonString, jsonString)
+		if err != nil {
+			t.Errorf("could not generate JSON by JSONString method by the following error: %v", err)
+		}
+
+		expectedJsonString := buildExpectedJson(scenery.messageSent)
+
+		if expectedJsonString != jsonString {
+			t.Errorf("the expected json (%v) does not match with returned (%v)", expectedJsonString, jsonString)
+		}
 	}
 }
 
