20cfdeaa0e6d37008a243daf01a5f0752251c198
albuquerque53 <gabrielalbuquerque-dev@hotmail.com>
2022-05-24 23:02:00 -0300

    feat: common database query methods

---
+++ internal/infra/database/db.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+)
+
+type DatabaseManager struct {
+	DB *sql.DB
+}
+
+func NewDatabaseManager() *DatabaseManager {
+	return &DatabaseManager{
+		DB: ConectToDatabase(),
+	}
+}
+
+func (dm *DatabaseManager) Query(ctx context.Context, q string, args ...interface{}) (*sql.Rows, error) {
+	stmt, err := dm.DB.Prepare(q)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	return stmt.QueryContext(ctx, args)
+}
+
+func (dm *DatabaseManager) QueryRow(ctx context.Context, q string, args ...interface{}) (*sql.Row, error) {
+	stmt, err := dm.DB.Prepare(q)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer stmt.Close()
+
+	return stmt.QueryRowContext(ctx, args), nil
+}
