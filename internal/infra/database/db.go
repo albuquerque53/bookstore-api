0722a0e75350e45eed416d6f83a0d23672e24869
albuquerque53 <gabrielalbuquerque-dev@hotmail.com>
2022-05-25 22:52:29 -0300

    fix: fixing errors in db helper

--- internal/infra/database/db.go
+++ internal/infra/database/db.go
@@ -16,16 +16,22 @@ func NewDatabaseManager() *DatabaseManager {
 }
 
 func (dm *DatabaseManager) Query(ctx context.Context, q string, args ...interface{}) (*sql.Rows, error) {
+	defer dm.DB.Close()
+
 	stmt, err := dm.DB.Prepare(q)
+
 	if err != nil {
 		return nil, err
 	}
+
 	defer stmt.Close()
 
-	return stmt.QueryContext(ctx, args)
+	return stmt.QueryContext(ctx, args...)
 }
 
 func (dm *DatabaseManager) QueryRow(ctx context.Context, q string, args ...interface{}) (*sql.Row, error) {
+	defer dm.DB.Close()
+
 	stmt, err := dm.DB.Prepare(q)
 
 	if err != nil {
