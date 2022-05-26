fcdba28e63e1b9ba949f5a827041a17dab1d0c06
albuquerque53 <gabrielalbuquerque-dev@hotmail.com>
2022-05-25 22:56:18 -0300

    feat: authors list route

--- internal/application/router/router.go
+++ internal/application/router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"bookstoreapi/internal/application/handler/author"
 	"bookstoreapi/internal/application/handler/misc"
 	"log"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 // Must handle all trafic of API
 func HandleRequests() {
 	http.HandleFunc("/health", misc.HealthCheck)
+	http.HandleFunc("/authors/list", author.ListAuthors)
 
 	log.Fatal(http.ListenAndServe(":2001", nil))
 }
