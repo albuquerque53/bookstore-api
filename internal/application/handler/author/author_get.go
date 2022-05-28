ccfcaa9f0ed0b782c853cef222728e23ec7fd523
albuquerque53 <gabrielalbuquerque-dev@hotmail.com>
2022-05-28 17:19:35 -0300

    refactor: using new get author id method

--- internal/application/handler/author/author_get.go
+++ internal/application/handler/author/author_get.go
@@ -7,14 +7,12 @@ import (
 	"bookstoreapi/internal/infra/repo"
 	"context"
 	"net/http"
-	"strconv"
-	"strings"
 )
 
 func GetAuthor(w http.ResponseWriter, req *http.Request) {
 	ctx := context.Background()
 
-	id, err := getAuthorId(w, req.URL.Path)
+	id, err := helper.GetAuthorId(w, req.URL.Path)
 
 	if err != nil {
 		return
@@ -34,18 +32,4 @@ func GetAuthor(w http.ResponseWriter, req *http.Request) {
 		Message: "ok",
 		Data:    author,
 	})
-}
-
-func getAuthorId(w http.ResponseWriter, urlPath string) (int, error) {
-	strId := strings.TrimPrefix(urlPath, "/authors/get/")
-
-	id, err := strconv.Atoi(strId)
-
-	if err != nil {
-		helper.HandleHttpError(w, 400, "invalid fields", nil)
-
-		return 0, err
-	}
-
-	return id, nil
 }
