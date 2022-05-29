4e1a25cc1e6bd74258925a9a89fb54025965dbd8
albuquerque53 <gabrielalbuquerque-dev@hotmail.com>
2022-05-29 10:42:39 -0300

    feat: route to update author informations

---
+++ internal/application/handler/author/author_edit.go
@@ -0,0 +1,41 @@
+package author
+
+import (
+	"bookstoreapi/internal/application/helper"
+	"bookstoreapi/internal/application/writer"
+	"bookstoreapi/internal/domain/author"
+	"bookstoreapi/internal/infra/repo"
+	"context"
+	"net/http"
+)
+
+func UpdateAuthor(w http.ResponseWriter, req *http.Request) {
+	ctx := context.Background()
+
+	id, err := helper.GetAuthorId(w, req.URL.Path)
+
+	if err != nil {
+		return
+	}
+
+	dto, err := getRequestBody(w, req)
+
+	if err != nil {
+		return
+	}
+
+	repo := repo.NewAuthorRepo()
+	authorEnt := author.NewAuthorEntity(repo)
+
+	err = authorEnt.UpdateAuthorById(ctx, id, dto)
+
+	if err != nil {
+		helper.HandleHttpError(w, 400, "error on author search", err)
+		return
+	}
+
+	writer.SendResponse(w, 204, writer.JSONResponse{
+		Message: "ok",
+		Data:    nil,
+	})
+}
