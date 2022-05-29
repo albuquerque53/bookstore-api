4df81d6f713d2efcc538cd9a561bacf6c4dddb50
albuquerque53 <gabrielalbuquerque-dev@hotmail.com>
2022-05-29 15:30:39 -0300

    feat: handler for update categories with id

---
+++ internal/application/handler/category/category_edit.go
@@ -0,0 +1,41 @@
+package category
+
+import (
+	"bookstoreapi/internal/application/helper"
+	"bookstoreapi/internal/application/writer"
+	"bookstoreapi/internal/domain/category"
+	"bookstoreapi/internal/infra/repo"
+	"context"
+	"net/http"
+)
+
+func UpdateCategory(w http.ResponseWriter, req *http.Request) {
+	ctx := context.Background()
+
+	id, err := helper.GetAuthorId(w, req.URL.Path)
+
+	if err != nil {
+		return
+	}
+
+	dto, err := getRequestBody(w, req)
+
+	if err != nil {
+		return
+	}
+
+	repo := repo.NewCategoryRepo()
+	categoryEnt := category.NewCategoryEntity(repo)
+
+	err = categoryEnt.UpdateCategoryById(ctx, id, dto)
+
+	if err != nil {
+		helper.HandleHttpError(w, 400, "error on category search", err)
+		return
+	}
+
+	writer.SendResponse(w, 204, writer.JSONResponse{
+		Message: "ok",
+		Data:    nil,
+	})
+}
