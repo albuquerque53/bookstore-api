dc4bd3bd74ddbf3bf900227c5a4dc268d263c3c5
albuquerque53 <gabrielalbuquerque-dev@hotmail.com>
2022-05-29 15:30:08 -0300

    feat: handler for search category by id

---
+++ internal/application/handler/category/category_get.go
@@ -0,0 +1,35 @@
+package category
+
+import (
+	"bookstoreapi/internal/application/helper"
+	"bookstoreapi/internal/application/writer"
+	"bookstoreapi/internal/domain/category"
+	"bookstoreapi/internal/infra/repo"
+	"context"
+	"net/http"
+)
+
+func GetCategory(w http.ResponseWriter, req *http.Request) {
+	ctx := context.Background()
+
+	id, err := helper.GetAuthorId(w, req.URL.Path)
+
+	if err != nil {
+		return
+	}
+
+	repo := repo.NewCategoryRepo()
+	categoryEnt := category.NewCategoryEntity(repo)
+
+	category, err := categoryEnt.GetCategoryById(ctx, id)
+
+	if err != nil {
+		helper.HandleHttpError(w, 400, "error on category search", err)
+		return
+	}
+
+	writer.SendResponse(w, 200, writer.JSONResponse{
+		Message: "ok",
+		Data:    category,
+	})
+}
