371b1d4fe780c94364fdb9ea2f62a6104957a152
albuquerque53 <gabrielalbuquerque-dev@hotmail.com>
2022-05-29 11:13:15 -0300

    fix: adjusting "books" table in migrations

--- internal/infra/database/migration/000003_book_schema.up.sql
+++ internal/infra/database/migration/000003_book_schema.up.sql
@@ -2,9 +2,9 @@ CREATE TABLE books(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(64) NOT NULL,
     author_id INT NOT NULL,
-    category_id INT NOT NULL,
+    category_id INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (author_id) REFERENCES authors(id),
-    FOREIGN KEY (category_id) REFERENCES categories(id)
+    FOREIGN KEY (author_id) REFERENCES authors(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE SET NULL
 );
