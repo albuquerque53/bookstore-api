859f61136f17b9b492f3c1e099a8328e1c588efe
albuquerque53 <gabrielalbuquerque-dev@hotmail.com>
2022-05-29 11:02:26 -0300

    refactor: improving error messages in connection.go

--- internal/infra/database/connection.go
+++ internal/infra/database/connection.go
@@ -13,11 +13,11 @@ func ConectToDatabase() *sql.DB {
 	db, err := sql.Open("mysql", dsn)
 
 	if err != nil {
-		log.Panicf("unexpected error on database connection: %v", err)
+		log.Panicf("error on database connection: %v", err)
 	}
 
 	if err = db.Ping(); err != nil {
-		log.Panicf("unexpected during database ping: %v", err)
+		log.Panicf("error during database ping: %v", err)
 	}
 
 	return db
